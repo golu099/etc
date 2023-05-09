710d0b577a520da2b6581909de2743d29ff8d459
golu099 <flavianegrete@gmail.com>
2023-05-09 15:23:28 -0400

    changes

--- Python/addHeader.py
+++ Python/addHeader.py
@@ -2,11 +2,10 @@
 import os
 import sys 
 import csv
-import pandas as pd 
-
+import pandas as pd
 
 with open(sys.argv[1], "rb") as text_file, open(sys.argv[2], "wb") as outfile:
-    data = pd.read_csv(sys.argv[1], header = 0, sep="/t", error_bad_lines=False, engine ='python')
+    data = pd.read_tsv(sys.argv[1], header = 0, sep="\t", error_bad_lines=False, engine ='python', skip_blank_lines=True, skipinitialspace= True)
     #grab basename of file
     basenamePath= print(os.path.splitext(sys.argv[1])[0])
     #make new empty column
